3709c6b82e4580bdadb0e95f9598ab6f57028bf1
strek-o <strek.oliwier@gmail.com>
2026-04-16 19:51:15 +0200

    Checkpoint

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-user3" 
+    resource_group_name  = "rg-user3"
     storage_account_name = "sauser3"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
@@ -20,16 +20,21 @@ terraform {
 }
 
 module "keyvault" {
-  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
+  source        = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
   keyvault_name = "kv-user3"
-  network_acls = {}
-  resource_group = {"name" = "rg-user3"}
+  network_acls  = {}
+  resource_group = {
+    name = "rg-user3"
+  location = "northeurope" }
 }
 
 module "mssql_server" {
   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=mssql_server/v1.0.0"
-  resource_group = {"name" = "rg-user3"}
-  sql_server_admin = "user3"
-  sql_server_name = "sqlsrv-user3"
+  resource_group = {
+    name     = "rg-user3"
+    location = "northeurope"
+  }
+  sql_server_admin   = "user3"
+  sql_server_name    = "sqlsrv-user3"
   sql_server_version = "12.0"
 }
